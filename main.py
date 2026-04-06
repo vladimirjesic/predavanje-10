b0cdf7a77ae326231fe9808bcac9843ea2ab5891
vladimirjesic <vladimirjesicc@gmail.com>
2026-04-06 19:51:54 +0200

    Added data.json and worked with it in main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#Komentar
+import json
+
+with open("data.json", "r") as file:
+    data = json.load(file)
+    data.append({
+        "name": "Petar Petrovic",
+        "age": 35,
+        "height": 190,
+        "gender": "male"
+    })
+
+print(data)
+
+with open("data.json", "w") as file:
+    json.dump(data, file, indent=4)
