ba49da44ae4dd8477100d6d609b32059cc10903e
vladimirjesic <vladimirjesicc@gmail.com>
2026-04-06 21:19:36 +0200

    Created methods empty_file and delete_file, also created test.json to test those methods

---
+++ domaci.py
@@ -0,0 +1,4 @@
+from methods import empty_file, delete_file
+
+empty_file("data/test.json")
+# delete_file("data/test.json")
--- methods.py
+++ methods.py
@@ -1,4 +1,6 @@
 import json
+import os
+
 
 def load_file(file_name):
     with open(file_name, "r") as file:
@@ -8,3 +10,10 @@ def load_file(file_name):
 def save_file(file_name, data_to_save):
     with open(file_name, "w") as file:
         json.dump(data_to_save, file, indent=4)
+
+def delete_file(file_name):
+    os.remove(file_name)
+
+def empty_file(file_name):
+    with open(file_name, "w") as file:
+        pass
